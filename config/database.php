3a8c74dec4ce152f4da6a32b8eee206052a53890
Brightness999 <brightness0804@gmail.com>
2020-07-27 23:23:58 +0300

    make it better

--- config/database.php
+++ config/database.php
@@ -71,7 +71,6 @@ return [
             'database' => ltrim($DATABASE_URL["path"], "/"),
             'username' => $DATABASE_URL["user"],
             'password' => $DATABASE_URL["pass"],
-            'charset' => 'utf8',
             'charset' => 'utf8mb4',
             'collation' => 'utf8mb4_unicode_ci',
             'prefix' => '',
