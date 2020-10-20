dd06445bb85dcee090eb93a9da47fcc5191f2e3f
Brightness999 <brightness0804@gmail.com>
2020-10-20 19:46:25 +0300

    Build fix.

--- resources/js/src/layouts/main/Main.vue
+++ resources/js/src/layouts/main/Main.vue
@@ -79,7 +79,7 @@ import TheCustomizer       from '@/layouts/components/customizer/TheCustomizer.v
 import TheFooter           from '@/layouts/components/TheFooter.vue'
 import themeConfig         from '@/../themeConfig.js'
 import VNavMenu            from '@/layouts/components/vertical-nav-menu/VerticalNavMenu.vue'
-import profilePhotoPosition        from '@/layouts/components/navbar/components/profilePhotoPosition.vue'
+import profilePhotoPosition        from '@/layouts/components/navbar/components/ProfilePhotoPosition.vue'
 import logOut              from '@/layouts/components/Logout.vue';
 
 const VxTour = () => import('@/components/VxTour.vue')
